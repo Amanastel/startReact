589fcdc0da8eaaea911a4f9ab81f6f1a777d39c4
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-12-21 17:34:45 +0530

    react code

---
+++ Day01/01ViteProject/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai";
+
+function App() {
+
+  const username = "aman like tea";
+
+  return (
+    <>
+      <Chai />
+      <h1>chai aur react {username}</h1>
+    </>
+  );
+}
+
+export default App;
